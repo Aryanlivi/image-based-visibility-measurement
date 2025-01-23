f018ac73dd5f5c790e3a54a42e9d0d27292326ea
Aryanlivi <aryanlivi@gmail.com>
2025-01-23 15:08:23 +0545

    Changed get_current_datetime

--- src/Utils.py
+++ src/Utils.py
@@ -2,4 +2,4 @@ from datetime import datetime
 import pytz
 def get_current_datetime(timezone_str="Asia/Kathmandu"):
     tz=pytz.timezone(timezone_str)
-    return datetime.now(tz=tz)
+    return datetime.now(tz=tz).replace(second=0,microsecond=0)
